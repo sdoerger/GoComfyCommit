4d4488ffd971519a14650b2edf9357ae1621b97a
sdoerger <stefan.doerger@stefan.doerger>
2022-08-11 17:08:42 +0200

    Check config file

--- main.go
+++ main.go
@@ -32,80 +32,79 @@ func main() {
 
 			}
 
-		} else {
-			if len(os.Args) == 2 {
-				commitText = os.Args[1]
-				changeType = ""
-			}
-			if len(os.Args) == 3 {
-				changeType = os.Args[1]
-				commitText = os.Args[2]
-			}
-
-			if commitText != "" {
-
-				// -------------------------------------------
-				// ------------  GET CURRENT BRANCH  ---------
-				// -------------------------------------------
-				runCurrentBranch := exec.Command("git", "branch", "--show-current")
-				currentBranch, err := runCurrentBranch.Output()
-
-				if err != nil {
-					fmt.Println(err.Error())
-					return
-				}
-
-				// -------------------------------------------
-				// ------------  ADD ALL FILES  --------------
-				// -------------------------------------------
-
-				runGitAddAll := exec.Command("git", "add", ".")
-				_, err = runGitAddAll.Output()
-
-				if err != nil {
-					fmt.Println(err.Error())
-					return
-				}
-
-				// -------------------------------------------
-				// ------------  COMMIT CMD MESSAGE + BRANCH
-				// -------------------------------------------
-				// Trimm branchname to 8, if needed
-				trimmedBranch := string(currentBranch[:len(currentBranch)-1])
-				if len(currentBranch) > 8 {
-					trimmedBranch = string(currentBranch[:8])
-				}
-
-				fullCommitText := ""
-
-				if os.Args != nil && len(os.Args) == 1 {
-					fullCommitText = string(commitText)
-				}
-
-				if os.Args != nil && len(os.Args) == 2 {
-					fullCommitText = string(commitText)
-				}
-
-				if os.Args != nil && len(os.Args) == 3 && changeType != "" {
-					fullCommitText = changeType + ": [" + string(trimmedBranch) + "] " + string(commitText)
-				}
-
-				runGitCommit := exec.Command("git", "commit", "-m"+fullCommitText)
-				_, err = runGitCommit.Output()
-
-				if err != nil {
-					fmt.Println(err.Error())
-					return
-				}
-
-				// Print the output
-				fmt.Println(string("✅: Commited:" + string(fullCommitText)))
-				fmt.Println("☕️: Just push")
-
-				return
-			}
 		}
 
+		if len(os.Args) == 2 {
+			commitText = os.Args[1]
+			changeType = ""
+		}
+		if len(os.Args) == 3 {
+			changeType = os.Args[1]
+			commitText = os.Args[2]
+		}
+
+		if commitText != "" {
+
+			// -------------------------------------------
+			// ------------  GET CURRENT BRANCH  ---------
+			// -------------------------------------------
+			runCurrentBranch := exec.Command("git", "branch", "--show-current")
+			currentBranch, err := runCurrentBranch.Output()
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			// -------------------------------------------
+			// ------------  ADD ALL FILES  --------------
+			// -------------------------------------------
+
+			runGitAddAll := exec.Command("git", "add", ".")
+			_, err = runGitAddAll.Output()
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			// -------------------------------------------
+			// ------------  COMMIT CMD MESSAGE + BRANCH
+			// -------------------------------------------
+			// Trimm branchname to 8, if needed
+			trimmedBranch := string(currentBranch[:len(currentBranch)-1])
+			if len(currentBranch) > 8 {
+				trimmedBranch = string(currentBranch[:8])
+			}
+
+			fullCommitText := ""
+
+			if os.Args != nil && len(os.Args) == 1 {
+				fullCommitText = string(commitText)
+			}
+
+			if os.Args != nil && len(os.Args) == 2 {
+				fullCommitText = string(commitText)
+			}
+
+			if os.Args != nil && len(os.Args) == 3 && changeType != "" {
+				fullCommitText = changeType + ": [" + string(trimmedBranch) + "] " + string(commitText)
+			}
+
+			runGitCommit := exec.Command("git", "commit", "-m"+fullCommitText)
+			_, err = runGitCommit.Output()
+
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			// Print the output
+			fmt.Println(string("✅: Commited:" + string(fullCommitText)))
+			fmt.Println("☕️: Just push")
+
+			return
+		}
 	} else {
 		fmt.Println("No Commit Message")
 	}
