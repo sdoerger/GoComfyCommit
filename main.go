62b876a5c423a9999cd7fba5ddc7e59710add2c7
sdoerger <doerger.stefan@gmail.com>
2022-08-12 14:55:46 +0200

    My CommitType: fix at Branch: main > My Message: Gotta do something

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 
 	// Check if there is a config.json file
 	_, hasNoConfig := helpers.OpenFileRead(setupPath)
-	if hasNoConfig == nil && len(*alias) > 0 {
+	if hasNoConfig == nil {
 
 		// // Get setup json
 		setupProfiles, err := helpers.SetupJson(setupPath)
